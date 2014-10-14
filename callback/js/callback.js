9542a617330eca963dbb968116d8cec4fdc670ea
Yrjö Kari-Koskinen <ykk@peruna.fi>
2014-10-14 23:49:52 +0300

    initial implementation with just local checking

---
+++ callback/js/callback.js
@@ -0,0 +1,60 @@
+function error(id, text) {
+	$("#" + id + "Error").text(text);
+}
+
+function validNumber(num) {
+	var intRegex = /^\d+$/;
+	return intRegex.test(num) && num.length == 10;
+}
+
+function checkMessage() {
+	var msg = $("#message").val().trim();
+	if (!msg) {
+		error("message", "Message is empty");
+		return false;
+	} else {
+		error("message", "");
+		return true;
+	}
+}
+
+function checkNumber() {
+	var num = $("#number").val().trim();
+	if (!validNumber(num)) {
+		error("number", "Invalid number");
+		return false;
+	} else {
+		error("number", "");
+		return true;
+	}
+}
+
+function checkForm() {
+	var numOk = checkNumber();
+	var msgOk = checkMessage();
+	return numOk && msgOk;
+}
+
+$(document).ready(function() {
+	$("#message").change(function(e) {
+		checkMessage();
+	});
+
+	$("#number").change(function(e) {
+		checkNumber();
+	});
+
+	$("#theForm").submit(function(event) {
+		event.preventDefault();
+
+		var formOk = checkForm();
+		var msg = $("#message").val().trim();
+		var num = $("#number").val().trim();
+
+		if (formOk) {
+			alert("Message '" + msg + "' was sent to " + num);
+			$("#message").val("");
+			$("#number").val("");
+		}
+	});
+});
